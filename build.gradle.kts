3f99bcf1854165261106f0817fb9df19da6c3008
Dimitris Boutas <dimitris.boutas@nowmax.be>
2022-11-11 21:37:36 +0100

    publish task

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,39 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.21"
     application
+    id("maven-publish")
 }
 
 group = "dimi.boo"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/dimitrisBoutas/publish-kotlin-gradle")
+            credentials {
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("dimiBoo") {
+            groupId = "dimi"
+            artifactId = "boo"
+            version = "1.0.0"
+            from(components["java"])
+        }
+    }
+
 }
 
 tasks.test {
